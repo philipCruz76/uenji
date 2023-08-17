18bd14144a771eee5565d036d0001d68d41d7f09
Ricardo Cruz <filipericardo076@gmail.com>
2023-08-17 17:49:00 +0100

    fixed socialAction bug

--- lib/actions/auth/socialAction.ts
+++ lib/actions/auth/socialAction.ts
@@ -1,16 +1,10 @@
 import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
 import { toast } from "react-hot-toast";
 
 export const socialAction = (action: string) => {
-  const router = useRouter();
   signIn(action, { redirect: false }).then((callback) => {
     if (callback?.error) {
       toast.error("Invalid Credentials");
-    }
-
-    if (callback?.ok) {
-      router.push("/");
     }
   });
 };
