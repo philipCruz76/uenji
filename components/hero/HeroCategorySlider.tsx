5399a7bb6d8f385c3d548afc64845386050dbdbc
Ricardo Cruz <filipericardo076@gmail.com>
2024-03-21 16:54:17 +0100

    - fixed visual bug on mobile

--- components/hero/HeroCategorySlider.tsx
+++ components/hero/HeroCategorySlider.tsx
@@ -2,8 +2,8 @@ import PhotoCaroussel from "@/components/ui/PhotoCaroussel";
 
 const HeroCategorySlider = () => {
   return (
-    <section className="container box-border flex h-[500px] w-full flex-col py-8 tablet:h-[700px] tablet:py-14 desktop:py-24">
-      <span className="flex w-[100dvw] px-2 text-3xl font-bold">
+    <section className="container box-border flex h-[500px] max-w-[100dvw] flex-col py-8 tablet:h-[700px] tablet:py-14 desktop:py-24">
+      <span className="flex max-w-[100dvw] px-2 text-3xl font-bold">
         Categorias em destaque
       </span>
 
