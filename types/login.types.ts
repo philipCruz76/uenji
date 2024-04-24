0f629e103c77a9b3d5718da2d33c79c12ae36c5e
Ricardo Cruz <filipericardo076@gmail.com>
2024-04-24 10:20:21 +0200

    Reverted fix - special characters are no longer mandatory but are allowed for use passwords

--- types/login.types.ts
+++ types/login.types.ts
@@ -13,8 +13,8 @@ export const LoginValidator = z.object({
     .string()
     .min(8, { message: "Password must be at least 8 characters long." })
     .max(20, { message: "Password must be at most 20 characters long." })
-    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[_\W])[a-zA-Z\d_\W]{8,}$/, {
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d_\W]{8,}$/, {
       message:
-        "Password must contain at least one uppercase letter, one lowercase letter,a special character and one number.",
+        "Password must contain at least one uppercase letter, one lowercase letter and one number.",
     }),
 });
