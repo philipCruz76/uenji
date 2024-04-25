510b88a2278cdf8052c91f05ce2caedfdda92dad
Ricardo Cruz <filipericardo076@gmail.com>
2024-04-25 19:27:06 +0200

    bug fix

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -72,7 +72,7 @@ export async function POST(request: Request) {
           deliveryTime.getDate() + parseInt(charge.metadata.gigDeliveryTime),
         );
 
-        //  BUG: order is being duplicated when running locally but not in production
+        /* BUG: order is being created somewhere else in the code, resulting in duplicate orders when this code is uncommented. Once the bug is fixed, uncomment this code.
         await db.order.create({
           data: {
             buyerId: charge.metadata.buyerId,
@@ -88,6 +88,7 @@ export async function POST(request: Request) {
             status: "active",
           },
         });
+        */
        
       }
     }
